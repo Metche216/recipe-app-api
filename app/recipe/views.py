da0f6c720e0dc5bbdd30ddb5748572167a0d843a
Metche216 <drmatiasetcheverry@gmail.com>
2025-03-05 18:38:36 -0400

    create new recipe done

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+"""
+Views for the recipe APIs
+"""
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Recipe
+from recipe import serializers
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """View for manage recipe APIs"""
+    serializer_class = serializers.RecipeDetailSerializer
+    queryset = Recipe.objects.all() #objects available for this viewset
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self): #override the get methode for this viewset
+        """Retrieve recipes for authenticated users"""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """Return the serializer class for request."""
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer): #override the behavior for when DRF saves a model in the viewset
+        """Create a new recipe"""
+        serializer.save(user = self.request.user)
+
